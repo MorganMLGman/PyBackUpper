dc3c432d5b58d9902f77d52ad1865b8f5c88bca4
MorganMLGman <cholody.kacper1998@gmail.com>
2022-12-22 18:59:24 +0100

    read_env

--- src/main.py
+++ src/main.py
@@ -1,18 +1,55 @@
 import os
+from pprint import pprint
+
+config = {
+    "RUNS_TO_KEEP": 7,
+    "DAYS_TO_RUN": [1, 3, 5, 7], #1: Monday .... 7: Sunday
+    "IF_COMPRESS": False,
+}
+
+def read_env():
+    try:
+        config["RUNS_TO_KEEP"] = int(os.environ['RUNS_TO_KEEP'])
+    except KeyError:
+        pass
+    except ValueError as e:
+        raise ValueError ("RUNS_TO_KEEP must be a number and be greater than 0") from e
+    if config["RUNS_TO_KEEP"] == 0:
+        raise ValueError ("RUNS_TO_KEEP must be a number and be greater than 0")
+    
+    try:
+        days = os.environ["DAYS_TO_RUN"]
+    except KeyError:
+        pass
+    
+    days = days.split(',')
+    days_array = []
+    for day in days:
+        try:
+            day = int(day)
+        except ValueError as e:
+            raise ValueError("DAYS_TO_RUN must be defined as string with comma as separator, eg. \"1,3,5\" where 1 is Monday, 3 is Wednesday and so on...")
+        
+        if day in range(1, 8) and day not in days_array:
+            days_array.append(day)
+        else:
+            raise ValueError("Allowed day vales are form 1 to 7, values must be unique")
+        
+    days_array.sort()
+    config["DAYS_TO_RUN"] = days_array           
+    
+    try:
+        config["IF_COMPRESS"] = True if os.environ['IF_COMPRESS'].lower() in ['true', 'yes', '1'] else False
+    except KeyError:
+        pass
+    except ValueError as e:
+        raise ValueError("IF_COMPRESS must be true or false") from e
+    
 
 def main():
-    try:
-        DAYS_TO_KEEP = int(os.environ['DAYS_TO_KEEP'])
-    except KeyError:
-        DAYS_TO_KEEP = 7
-        
-    try:
-        IF_COMPRESS = bool(os.environ['IS_COMPRESS'])
-    except KeyError:
-        IF_COMPRESS = True
+    read_env()
     
-    print(DAYS_TO_KEEP)
-    print(IF_COMPRESS)
+    print(config)
 
 if __name__ == "__main__":
     main()
